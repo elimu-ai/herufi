306ae2ccf5f344e7b0348f8635f34f6e3e7f5b03
Nya Elimu <nya@elimu.ai>
2020-07-03 17:34:03 +0800

    #2 Display list of letters
    - Added letter view

--- app/src/main/java/ai/elimu/herufi/ui/LetterListActivity.java
+++ app/src/main/java/ai/elimu/herufi/ui/LetterListActivity.java
@@ -2,9 +2,14 @@ package ai.elimu.herufi.ui;
 
 import android.os.Bundle;
 import android.util.Log;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.widget.TextView;
 
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
+
+import com.google.android.flexbox.FlexboxLayout;
 
 import java.util.List;
 
@@ -14,12 +19,16 @@ import ai.elimu.model.v2.gson.content.LetterGson;
 
 public class LetterListActivity extends AppCompatActivity {
 
+    private FlexboxLayout flexboxLayout;
+
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         Log.i(getClass().getName(), "onCreate");
         super.onCreate(savedInstanceState);
 
         setContentView(R.layout.activity_letter_list);
+
+        flexboxLayout = findViewById(R.id.letter_list_flexbox_layout);
     }
 
     @Override
@@ -30,6 +39,13 @@ public class LetterListActivity extends AppCompatActivity {
         List<LetterGson> letterGsons = ContentProviderHelper.getLetterGsons(getApplicationContext());
         Log.i(getClass().getName(), "letterGsons.size(): " + letterGsons.size());
 
-        //
+        // Create a view for each letter in the list
+        flexboxLayout.removeAllViews();
+        for (LetterGson letterGson : letterGsons) {
+            View letterView = LayoutInflater.from(this).inflate(R.layout.activity_letter_list_letter_view, flexboxLayout, false);
+            TextView textView = letterView.findViewById(R.id.letter_view_text_view);
+            textView.setText(letterGson.getText());
+            flexboxLayout.addView(letterView);
+        }
     }
 }

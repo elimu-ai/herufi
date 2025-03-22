2085391c3efb1ab65d00e3aba8ae37ba54521575
Jo G. <1451036+jo-elimu@users.noreply.github.com>
2025-03-22 19:45:13 +0700

    test: android emulator workflow

--- app/src/androidTest/java/ai/elimu/herufi/ExampleInstrumentedTest.java
+++ app/src/androidTest/java/ai/elimu/herufi/ExampleInstrumentedTest.java
@@ -21,6 +21,6 @@ public class ExampleInstrumentedTest {
     public void useAppContext() {
         // Context of the app under test.
         Context appContext = InstrumentationRegistry.getInstrumentation().getTargetContext();
-        assertEquals("ai.elimu.herufi", appContext.getPackageName());
+        assertEquals("ai.elimu.herufi.debug", appContext.getPackageName());
     }
 }

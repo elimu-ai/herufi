7afa05fe8aef6f55c6f2bc57463be55b1374580d
Tuan Nguyen <tuancoltech@gmail.com>
2025-04-09 16:05:06 +0700

    Convert LetterSoundListActivity to Kotlin

--- app/src/main/java/ai/elimu/herufi/ui/LetterSoundListActivity.kt
+++ app/src/main/java/ai/elimu/herufi/ui/LetterSoundListActivity.kt
@@ -1,78 +1,85 @@
-package ai.elimu.herufi.ui;
+package ai.elimu.herufi.ui
 
-import android.os.Bundle;
-import android.speech.tts.TextToSpeech;
-import android.util.Log;
-import android.view.LayoutInflater;
-import android.view.View;
-import android.widget.TextView;
+import ai.elimu.analytics.utils.LearningEventUtil
+import ai.elimu.content_provider.utils.ContentProviderUtil.getAllLetterSoundGsons
+import ai.elimu.herufi.BaseApplication
+import ai.elimu.herufi.BuildConfig
+import ai.elimu.herufi.R
+import ai.elimu.model.v2.gson.content.LetterGson
+import ai.elimu.model.v2.gson.content.SoundGson
+import android.os.Bundle
+import android.speech.tts.TextToSpeech
+import android.util.Log
+import android.view.LayoutInflater
+import android.view.View
+import android.widget.TextView
+import androidx.appcompat.app.AppCompatActivity
+import com.google.android.flexbox.FlexboxLayout
+import java.util.stream.Collectors
 
-import androidx.appcompat.app.AppCompatActivity;
+class LetterSoundListActivity : AppCompatActivity() {
+    private var flexboxLayout: FlexboxLayout? = null
 
-import com.google.android.flexbox.FlexboxLayout;
+    public override fun onCreate(savedInstanceState: Bundle?) {
+        Log.i(javaClass.name, "onCreate")
+        super.onCreate(savedInstanceState)
 
-import java.util.List;
-import java.util.stream.Collectors;
+        setContentView(R.layout.activity_letter_sound_list)
 
-import ai.elimu.analytics.utils.LearningEventUtil;
-import ai.elimu.content_provider.utils.ContentProviderUtil;
-import ai.elimu.herufi.BaseApplication;
-import ai.elimu.herufi.BuildConfig;
-import ai.elimu.herufi.R;
-import ai.elimu.model.v2.gson.content.LetterGson;
-import ai.elimu.model.v2.gson.content.LetterSoundGson;
-import ai.elimu.model.v2.gson.content.SoundGson;
-
-public class LetterSoundListActivity extends AppCompatActivity {
-
-    private FlexboxLayout flexboxLayout;
-
-    @Override
-    public void onCreate(Bundle savedInstanceState) {
-        Log.i(getClass().getName(), "onCreate");
-        super.onCreate(savedInstanceState);
-        
-        setContentView(R.layout.activity_letter_sound_list);
-
-        flexboxLayout = findViewById(R.id.letter_sound_list_flexbox_layout);
+        flexboxLayout = findViewById(R.id.letter_sound_list_flexbox_layout)
     }
 
-    @Override
-    protected void onStart() {
-        Log.i(getClass().getName(), "onStart");
-        super.onStart();
+    override fun onStart() {
+        Log.i(javaClass.name, "onStart")
+        super.onStart()
 
-        List<LetterSoundGson> letterSoundGsons = ContentProviderUtil.INSTANCE.getAllLetterSoundGsons(getApplicationContext(), BuildConfig.CONTENT_PROVIDER_APPLICATION_ID);
-        Log.i(getClass().getName(), "letterSoundGsons.size(): " + letterSoundGsons.size());
+        val letterSoundGsons = getAllLetterSoundGsons(
+            applicationContext, BuildConfig.CONTENT_PROVIDER_APPLICATION_ID
+        )
+        Log.i(javaClass.name, "letterSoundGsons.size(): " + letterSoundGsons.size)
 
         // Create a view for each letter-sound correspondence in the list
-        flexboxLayout.removeAllViews();
-        for (final LetterSoundGson letterSoundGson : letterSoundGsons) {
-            View letterSoundView = LayoutInflater.from(this).inflate(R.layout.activity_letter_sound_list_letter_view, flexboxLayout, false);
+        flexboxLayout!!.removeAllViews()
+        for (letterSoundGson in letterSoundGsons) {
+            val letterSoundView = LayoutInflater.from(this)
+                .inflate(R.layout.activity_letter_sound_list_letter_view, flexboxLayout, false)
 
-            TextView textView = letterSoundView.findViewById(R.id.letter_sound_view_text_view);
-            String sounds = letterSoundGson.getSounds().stream().map(SoundGson::getValueIpa).collect(Collectors.joining());
-            String letters = letterSoundGson.getLetters().stream().map(LetterGson::getText).collect(Collectors.joining());
-            textView.setText("/" + sounds + "/\n⬇\n\"" + letters + "\"");
+            val textView = letterSoundView.findViewById<TextView>(R.id.letter_sound_view_text_view)
+            val sounds =
+                letterSoundGson.sounds.stream().map { obj: SoundGson -> obj.valueIpa }.collect(
+                    Collectors.joining()
+                )
+            val letters =
+                letterSoundGson.letters.stream().map { obj: LetterGson -> obj.text }.collect(
+                    Collectors.joining()
+                )
+            textView.text = "/$sounds/\n⬇\n\"$letters\""
 
             // Play sound when pressed
-            letterSoundView.setOnClickListener(new View.OnClickListener() {
-                @Override
-                public void onClick(View v) {
-                    Log.i(getClass().getName(), "letterView onClick");
+            letterSoundView.setOnClickListener(object : View.OnClickListener {
+                override fun onClick(v: View) {
+                    Log.i(javaClass.name, "letterView onClick")
 
-                    Log.i(getClass().getName(), "letterSoundGson.getId(): '" + letterSoundGson.getId() + "'");
+                    Log.i(javaClass.name, "letterSoundGson.getId(): '" + letterSoundGson.id + "'")
 
-                    BaseApplication baseApplication = (BaseApplication) getApplication();
-                    TextToSpeech tts = baseApplication.getTTS();
-                    tts.speak(letters, TextToSpeech.QUEUE_FLUSH, null, "letter_sound_" + letterSoundGson.getId());
+                    val baseApplication = application as BaseApplication
+                    val tts = baseApplication.tts
+                    tts.speak(
+                        letters,
+                        TextToSpeech.QUEUE_FLUSH,
+                        null,
+                        "letter_sound_" + letterSoundGson.id
+                    )
 
                     // Report learning event to the Analytics application (https://github.com/elimu-ai/analytics)
-                    LearningEventUtil.reportLetterSoundLearningEvent(letterSoundGson, getApplicationContext(), BuildConfig.ANALYTICS_APPLICATION_ID);
+                    LearningEventUtil.reportLetterSoundLearningEvent(
+                        letterSoundGson,
+                        applicationContext, BuildConfig.ANALYTICS_APPLICATION_ID
+                    )
                 }
-            });
+            })
 
-            flexboxLayout.addView(letterSoundView);
+            flexboxLayout!!.addView(letterSoundView)
         }
     }
 }

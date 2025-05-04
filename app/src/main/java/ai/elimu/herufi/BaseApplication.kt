8c7df09d36fea0e1793a9db0f98c382521dd6fce
tuancoltech <tuancoltech@gmail.com>
2025-05-04 22:52:58 +0700

    Use TextToSpeechViewModel to play audio

--- app/src/main/java/ai/elimu/herufi/BaseApplication.kt
+++ app/src/main/java/ai/elimu/herufi/BaseApplication.kt
@@ -1,40 +1,7 @@
 package ai.elimu.herufi
 
 import android.app.Application
-import android.speech.tts.TextToSpeech
-import android.speech.tts.TextToSpeech.OnInitListener
-import android.util.Log
+import dagger.hilt.android.HiltAndroidApp
 
-class BaseApplication : Application() {
-    var tts: TextToSpeech? = null
-
-    override fun onCreate() {
-        Log.i(javaClass.name, "onCreate")
-        super.onCreate()
-
-        // Initialize Text-to-Speech
-        tTS
-    }
-
-    val tTS: TextToSpeech
-        get() {
-            if (tts == null) {
-                tts = TextToSpeech(applicationContext, object : OnInitListener {
-                    override fun onInit(status: Int) {
-                        Log.i(javaClass.name, "TextToSpeech onInit")
-                    }
-                })
-            }
-            return tts as TextToSpeech
-        }
-
-    override fun onTerminate() {
-        Log.i(javaClass.name, "onTerminate")
-        super.onTerminate()
-
-        // Release the resources used by the TextToSpeech engine
-        if (tts != null) {
-            tts!!.shutdown()
-        }
-    }
-}
+@HiltAndroidApp
+class BaseApplication : Application()
